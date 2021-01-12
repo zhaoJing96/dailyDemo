9f4369362f789a687cb97d02f88f254e83f603f9
zhao.jing <zhao.jing@dhwork.cn>
2021-01-12 11:33:22 +0800

    文字移动update

--- fallGroundPage/js/common.js
+++ fallGroundPage/js/common.js
@@ -1,18 +1,18 @@
+let winheight = window.innerHeight; // 获取屏幕可视区域高度
 /**
  * 文字动效 上移
- * @param {*} winHeight 屏幕可视区域高度
  * @param {*} domNode 需要添加动效的节点 [dom] array类型
  */
-function setTextTransFormY(winHeight, domNode) {
+function setTextTransFormY(domNode) {
     for (let i = 0; i < domNode.length; i++) {
         const item = domNode[i];
         if(item){
             let domTopHei = item && item.getBoundingClientRect().top;
-            if (domTopHei <= winHeight && domTopHei > 0) {
+            if (domTopHei <= winheight && domTopHei > 0) {
                 if (item && !item.classList.contains('index_text_animate')) {
                     item.classList.add('index_text_animate');
                 }
-            } else if (domTopHei > winHeight) {
+            } else if (domTopHei > winheight) {
                 if (item && item.classList.contains('index_text_animate')) {
                     item.classList.remove('index_text_animate');
                 }
